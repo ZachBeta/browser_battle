ce0fe8a34f5f06bddab8a79764190a36b4b3b7b9
Zach Morek <zmorek@gmail.com>
2013-08-20 23:41:50 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrowserBattle::Application.config.secret_key_base = 'ff4b76dd882268f12ffa867686b1d97ee9767f1fcc5d434deea912ae72221fc99e24d6539f9a157af7cfef3d6ccfa7512d3372e899423d3087a482c96413f1d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BrowserBattle::Application.config.session_store :cookie_store, key: '_browser_battle_session'
